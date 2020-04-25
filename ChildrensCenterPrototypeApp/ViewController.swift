1992a440ebc52e4a579aec8bc9ae98cc6cc16f98
Lilya Parilyak <lip@meshtech.no>
2020-04-25 13:54:52 +0300

    Initial Commit

---
+++ ChildrensCenterPrototypeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChildrensCenterPrototypeApp
+//
+//  Created by Lilya on 25.04.2020.
+//  Copyright © 2020 Lilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
